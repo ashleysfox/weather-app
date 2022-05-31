3af087eb1c5fe7295ec707e48ae3757a8139d809
ashfox <ashleyfox.design@gmail.com>
2022-05-30 17:43:05 -0700

    Made API global item
    Also began work on unit conversion

--- script/app.js
+++ script/app.js
@@ -35,10 +35,9 @@ console.log(newDate);
 
 //Show Weather
 function showWeather(response) {
-    console.log(response.data.name);
+    console.log(response);
     let currentTemp = Math.round(response.data.main.temp);
-    let tempH1 = document.querySelector("h1");
-    tempH1.innerHTML = `${currentTemp}°`;
+    tempHeading.innerHTML = `${currentTemp}°`;
     let cityHeading = document.querySelector("#city-heading");
     let searchedhCity = `${response.data.name}`;
     cityHeading.innerHTML = `${searchedhCity}, ${response.data.sys.country}`;
@@ -62,28 +61,29 @@ function showWeather(response) {
     weatherIcon.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
   }
 
-//Show current location weather by form search field
+//Show location weather by form search field
 let searchField = document.querySelector("#search-form");
 function weatherDataSearch(event) {
   event.preventDefault();
   let newSearchedCity = document.querySelector("#search-field").value;
-  let searchApiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${newSearchedCity}&appid=${apiKey}&units=${unit}`;
-  axios.get(searchApiUrl).then(showWeather);
+  apiUrl = apiUrl + `&q=${newSearchedCity}`;
+  axios.get(apiUrl).then(showWeather);
 }
 searchField.addEventListener("submit", weatherDataSearch);
 
-//Show current location weather on button click
+//Show current location weather on button click + load
 function getLocation() {
   navigator.geolocation.getCurrentPosition(showPosition);
   alert("Fetching location...");
   function showPosition(position) {
     let lat = position.coords.latitude;
     let lon = position.coords.longitude;
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${unit}`;
+    apiUrl = apiUrl + `&lat=${lat}&lon=${lon}`;
     axios.get(apiUrl).then(showWeather);
 }
 }
 
+//Show current location weather on button click 
 let locationButton = document.querySelector("#current-location");
 locationButton.addEventListener("click", getLocation);
 
@@ -98,6 +98,25 @@ bkgGradient.classList.remove("today-section-day");
 bkgGradient.classList.add("today-section-night");
 } 
 
+//Celsius to Fahrenheit Conversion 
+function showCelsiusTemp(event){
+  event.preventDefault();
+  console.log(apiURL);
+  apiURL.replace("imperial", "metric");
+  console.log(apiURL);
+}
+
+function showFahrenheitTemp(event){
+  event.preventDefault();
+}
+
+let fahrenheitLink = document.querySelector("#fahrenheit");
+let celsiusLink = document.querySelector("#celsius");
+fahrenheitLink.addEventListener("click", showFahrenheitTemp);
+celsiusLink.addEventListener("click", showCelsiusTemp);
+
 //Global Items
 let unit = "imperial"
 let apiKey = "d99d532213301980bc66856f61cac4e9";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?&appid=${apiKey}&units=${unit}`;
+let tempHeading = document.querySelector("#current-temperature");
